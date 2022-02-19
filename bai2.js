6a7623895412eb5dc5ad36a353c230ae58066089
PhamVanSang2601 <96646248+PhamVanSang2601@users.noreply.github.com>
2022-02-19 20:31:36 +0700

    them loai tru

--- bai2.js
+++ bai2.js
@@ -2,10 +2,19 @@ const submit = document.getElementById("submit").onclick = function () {
     const x = +document.getElementById("x").value;
     const n = +document.getElementById("n").value;
     const result2 = document.getElementById("result2");
-    let s = 0;
-    for(let i = 1; i <= n; i++){
-        s += Math.pow(x, i);
+    let s = 0 ;
+    if (n >= 1){
+        for (let i = 1 ; i <= n; i++){
+            s += Math.pow(x, i);
+        }
+    } else if (0 <= n  && n < 1){
+        s += x + Math.pow(x,n);
+    } else if (n < 0){
+        for (;n < 0; n++){
+            s += Math.pow(x,n)
+        }
     }
     result2.style.display = "block";
     result2.innerHTML += `${s}`
+    
 }
--- bai3.js
+++ bai3.js
@@ -2,10 +2,19 @@ const submit2 = document.getElementById("submit2").onclick = function (){
     const y = +document.getElementById("y").value;
     const result3 = document.getElementById("result3");
     let f = 1;
-    for(let i = 1; i <= y; i++){
-        f = f * i;
+    if (y > 0 && y % 1 === 0){
+        for(let i = 1; i <= y; i++){
+            f = f * i;
+        }
+    } else if (y === 0){
+        f = 1;  
+    } else if (y < 0 || y % 1 !== 0){
+        f = 0;
     }
-    console.log(f);
     result3.style.display = "block";
-    result3.innerHTML += `${f}`;
+    if (f === 0){
+        result3.innerHTML += `N\A`;
+    } else {
+        result3.innerHTML += `${f}`;
+    }
 }
